e12c7d2ea03299e6e05900a1e54f9b3f62fdd5fa
SavindaJayasekara <thantrige32@gmail.com>
2023-11-24 13:36:54 +0530

    add override do get

--- Hello-java-EE/src/main/java/lk/ijse/hello/api/HelloServlet.java
+++ Hello-java-EE/src/main/java/lk/ijse/hello/api/HelloServlet.java
@@ -1,4 +1,15 @@
 package lk.ijse.hello.api;
 
-public class HelloServlet {
+//java EE use krana nisa javax walin enne
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
