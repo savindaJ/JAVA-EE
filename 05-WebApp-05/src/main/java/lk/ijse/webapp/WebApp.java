a31c02c5a0bb7879232523ed20459ab4dc6e85ad
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-11-28 11:12:20 +0530

    build and deployment

---
+++ 05-WebApp-05/src/main/java/lk/ijse/webapp/WebApp.java
@@ -0,0 +1,17 @@
+package lk.ijse.webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WebApp extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("get !");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>Hello JAVA EE !");
+    }
+}
