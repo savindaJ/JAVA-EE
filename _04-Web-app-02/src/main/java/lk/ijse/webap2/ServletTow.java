d9eaa9b827bea5a11544ec4d5e224755d970cb94
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-01 12:12:47 +0530

    add requast !

--- _04-Web-app-02/src/main/java/lk/ijse/webap2/ServletTow.java
+++ _04-Web-app-02/src/main/java/lk/ijse/webap2/ServletTow.java
@@ -14,7 +14,31 @@ import java.io.IOException;
 public class ServletTow extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("<h1>Servlet 1 !</h1>");
-        System.out.println("Hello ee 2 !");
+        resp.getWriter().println("<h1>Servlet 1 !</h1>");
+        resp.getWriter().println("<h1>HTTP GET !</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Servlet 1 !</h1>");
+        resp.getWriter().println("<h1>HTTP POST !</h1>");
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Servlet 1 !</h1>");
+        resp.getWriter().println("<h1>HTTP OPTION !</h1>");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Servlet 1 !</h1>");
+        resp.getWriter().println("<h1>HTTP DELETE !</h1>");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Servlet 1 !</h1>");
+        resp.getWriter().println("<h1>HTTP PUT !</h1>");
     }
 }
