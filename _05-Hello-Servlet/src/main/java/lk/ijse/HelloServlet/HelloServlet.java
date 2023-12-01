0d04274deb1a2eec5a5fbd8f1f9a96b28696331f
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-01 13:22:07 +0530

    add new modeul !

---
+++ _05-Hello-Servlet/src/main/java/lk/ijse/HelloServlet/HelloServlet.java
@@ -0,0 +1,19 @@
+package lk.ijse.HelloServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : savindaJ
+ * @date : 12/1/2023
+ * @since : 0.1.0
+ **/
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hello !");
+    }
+}
