b0790e938e815611adbbc7a5456b7db2bee9cf16
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-01 09:15:08 +0530

    add web app 2

--- 03-Hello-EE-MVN/src/main/java/lk/ijse/hello/api/HelloServlet.java
+++ 03-Hello-EE-MVN/src/main/java/lk/ijse/hello/api/HelloServlet.java
@@ -10,6 +10,6 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("<h1>Hello JAVA EE / MVN !");
+        resp.getWriter().println("<h1>Hello JAVA EE 2 !");
     }
 }
