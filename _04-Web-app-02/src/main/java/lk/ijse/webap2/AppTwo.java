3b489f553b0dadbf94c5c46f9391803679d32a43
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-01 09:40:41 +0530

    add web app 2

--- _04-Web-app-02/src/main/java/lk/ijse/webap2/AppTwo.java
+++ _04-Web-app-02/src/main/java/lk/ijse/webap2/AppTwo.java
@@ -1,6 +1,7 @@
 package lk.ijse.webap2;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import java.io.IOException;
  * @date : 12/1/2023
  * @since : 0.1.0
  **/
+//@WebServlet(urlPatterns = "/test")
 public class AppTwo extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
