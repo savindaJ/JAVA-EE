937ce8c4d9037ccda1c7f9a3de2ddd083f653d17
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-01 15:08:00 +0530

    add servlet configuration !

--- _05-Hello-Servlet/src/main/java/lk/ijse/HelloServlet/HelloServlet.java
+++ _05-Hello-Servlet/src/main/java/lk/ijse/HelloServlet/HelloServlet.java
@@ -1,5 +1,6 @@
 package lk.ijse.HelloServlet;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,5 +17,16 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("<h1>get method invoke</h1>");
         System.out.println("get() invoke !");
+//        get servlet config !
+        ServletConfig config = getServletConfig();
+//        and get servlet config name parameter value
+//        this value stored in web.xml file !
+        String city = config.getInitParameter("city");
+        System.out.println(city);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init created !");
     }
 }
