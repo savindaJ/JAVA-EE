2d2710b4adc02a14eee6fc8d8b1f59ca270b97b7
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2023-12-03 21:09:00 +0530

    add sample

--- _05-Hello-Servlet/src/main/java/lk/ijse/HelloServlet/ThreadServe.java
+++ _05-Hello-Servlet/src/main/java/lk/ijse/HelloServlet/ThreadServe.java
@@ -20,5 +20,6 @@ public class ThreadServe extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.printf("post !");
+//        sample
     }
 }
