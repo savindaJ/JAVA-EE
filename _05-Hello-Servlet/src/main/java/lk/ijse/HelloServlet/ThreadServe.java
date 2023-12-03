497ba376d34c64f15989f16965908bdb2a705661
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2023-12-03 18:08:37 +0530

    add new class

--- _05-Hello-Servlet/src/main/java/lk/ijse/HelloServlet/ThreadServe.java
+++ _05-Hello-Servlet/src/main/java/lk/ijse/HelloServlet/ThreadServe.java
@@ -16,4 +16,9 @@ public class ThreadServe extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.printf("called !");
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.printf("post !");
+    }
 }
