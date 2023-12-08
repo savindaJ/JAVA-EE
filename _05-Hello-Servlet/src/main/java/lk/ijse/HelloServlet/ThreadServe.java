0e6b88f156f57c0d3b93cd6dd3f4906200bbba1e
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2023-12-08 09:05:03 +0530

    add components

--- _05-Hello-Servlet/src/main/java/lk/ijse/HelloServlet/ThreadServe.java
+++ _05-Hello-Servlet/src/main/java/lk/ijse/HelloServlet/ThreadServe.java
@@ -1,5 +1,6 @@
 package lk.ijse.HelloServlet;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +16,9 @@ public class ThreadServe extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.print("called !");
+        ServletContext servletContext = getServletContext();
+        String name = servletContext.getInitParameter("name");
+        System.out.println("Servlet 3 context : "+name);
     }
 
     @Override
