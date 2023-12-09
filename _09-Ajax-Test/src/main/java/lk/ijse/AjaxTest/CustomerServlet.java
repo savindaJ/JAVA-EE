db4bc0acae74ff4c13d23c659bd6f15cb042ec76
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2023-12-09 18:25:51 +0530

    add ajax test

---
+++ _09-Ajax-Test/src/main/java/lk/ijse/AjaxTest/CustomerServlet.java
@@ -0,0 +1,36 @@
+package lk.ijse.AjaxTest;
+
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+/**
+ * @author : savindaJ
+ * @date : 12/1/2023
+ * @since : 0.1.0
+ **/
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("get all customer !");
+
+            JsonObjectBuilder obj = Json.createObjectBuilder();
+
+            obj.add("state", "OK");
+            obj.add("message", "Successfully Loaded..!");
+            obj.add("data", "");
+            resp.setStatus(200);
+
+            resp.getWriter().print(obj.build());
+
+    }
+}
