6f82dc20ace9346b85db4f1768edf6ed59e58311
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2023-12-22 13:26:13 +0530

    add mapping class

--- _10-Mapping-Specification/src/main/java/lk/ijse/mappingSpecificarion/Mapping.java
+++ _10-Mapping-Specification/src/main/java/lk/ijse/mappingSpecificarion/Mapping.java
@@ -8,13 +8,24 @@
 
 package lk.ijse.mappingSpecificarion;
 
-import javax.servlet.http.HttpServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 /**
  * @author : savindaJ
  * @date : 12/1/2023
  * @since : 0.1.0
  **/
-public class Mapping extends HttpServlet {
 
+@WebServlet(urlPatterns = "/")
+public class Mapping extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("get !");
+    }
 }
