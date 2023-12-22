334c12f6b35f898c152702c1ea7fc03ff0c3fd03
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2023-12-22 13:22:23 +0530

    add mapping specification !

---
+++ _10-Mapping-Specification/src/main/java/lk/ijse/mappingSpecificarion/Mapping.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package lk.ijse.mappingSpecificarion;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author : savindaJ
+ * @date : 12/1/2023
+ * @since : 0.1.0
+ **/
+public class Mapping extends HttpServlet {
+
+}
