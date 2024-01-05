e42c057ea86bd03f154ab955b3f7d4ea1a4e7122
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-01-05 15:06:03 +0530

    add json type and get paramiters

--- _12-Json/src/main/java/lk/ijse/json/Test.java
+++ _12-Json/src/main/java/lk/ijse/json/Test.java
@@ -32,51 +32,48 @@ public class Test extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("post");
+        //        client send to json to not working !
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        String salary = req.getParameter("salary");
+
+        System.out.println(salary + name + id + address);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+//        client send to json to not working !
+
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        String salary = req.getParameter("salary");
+
+        System.out.println(salary + name + id + address);
+
+
         ArrayList<String> strings = new ArrayList<>();
-
         ArrayList<Customer> list = new ArrayList<>();
-
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection root = DriverManager.getConnection
                     ("jdbc:mysql://localhost:3306/web_test", "root", "80221474");
             PreparedStatement pstm = root.prepareStatement("SELECT * FROM customer");
             ResultSet set = pstm.executeQuery();
-
             while (set.next()) {
-
-                list.add(new Customer(set.getString(1),set.getString(2),set.getString(3),set.getDouble(4)));
-
-
+                list.add(new Customer(set.getString(1), set.getString(2), set.getString(3), set.getDouble(4)));
                 String start = "{";
-
-                /*System.out.println("ID :" + set.getString(1) + "," +
-                        "NAME :" + set.getString(2) + "," +
-                        "Address :" + set.getString(3) + "," +
-                        "Salary :" + set.getDouble(4)
-                );*/
-
-                String body = "\"id\"" + ":" +"\""+ set.getString(1)+"\"" + "," + "\"name\"" + ":" + "\""+ set.getString(2)+"\"" + "," +
-                        "\"address\" :" +"\""+ set.getString(3) +"\""+ ","+"\"salary\" :"+"\""+set.getDouble(4);
-
+                String body = "\"id\"" + ":" + "\"" + set.getString(1) + "\"" + "," + "\"name\"" + ":" + "\"" + set.getString(2) + "\"" + "," +
+                        "\"address\" :" + "\"" + set.getString(3) + "\"" + "," + "\"salary\" :" + "\"" + set.getDouble(4);
                 String end = "}";
-
-                String json = start+body+end;
-
+                String json = start + body + end;
                 strings.add(json);
             }
             resp.setContentType("application/json");
-
             resp.getWriter().println(new Gson().toJson(list));
-
-            resp.getWriter().println(strings);
-
-
+//            resp.getWriter().println(strings);
         } catch (Exception e) {
             resp.getWriter().println("<h1>Error !!!</h1>");
             throw new RuntimeException(e);
