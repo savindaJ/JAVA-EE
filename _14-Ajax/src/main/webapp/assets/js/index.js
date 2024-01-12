049c3633710971c07c0568471fb536245828446f
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-01-12 16:40:32 +0530

    add data types and xml request send

--- _14-Ajax/src/main/webapp/assets/js/index.js
+++ _14-Ajax/src/main/webapp/assets/js/index.js
@@ -4,6 +4,14 @@ $('#btnSave').on('click', function () {
     http.open('get','customer.json');
     http.send();
     console.log(http.responseText);
+
+    for (let cus of http.responseText)
+
+    $('#order-table').append(`
+            <tr>
+                <td>${cus.id}</td>
+            </tr>
+    `);
 });
 
 
